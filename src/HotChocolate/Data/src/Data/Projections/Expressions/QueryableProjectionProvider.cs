5f17a11497e3b83dc0ea9727e3272c37c4659187
AntonC9018 <anton.curm@gmail.com>
2023-03-31 11:46:39 +0300

    remove the key

--- src/HotChocolate/Data/src/Data/Projections/Expressions/QueryableProjectionProvider.cs
+++ src/HotChocolate/Data/src/Data/Projections/Expressions/QueryableProjectionProvider.cs
@@ -140,9 +140,6 @@ public class QueryableProjectionProvider : ProjectionProvider
 
             var projection = visitorContext.Project<TEntityType>();
 
-            context.LocalContextData = context.LocalContextData.SetItem(
-                "ProjectionVisitorContext", visitorContext);
-
             return ApplyToResult(input, projection);
         };
 }
